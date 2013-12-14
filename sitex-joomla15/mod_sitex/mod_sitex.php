1257b7303dbe77ef7313e102a5e279e0f07284c7
cpulse <alex@alxnb.local>
2013-12-14 21:47:59 +0200

    Sitex v1.0 for Joomla! 1.5, 2.x, 3.x

---
+++ sitex-joomla15/mod_sitex/mod_sitex.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * mod_sitex
+ *
+ * Simple text module
+ *
+ * @version 1.0
+ * @author Creative Pulse
+ * @copyright Creative Pulse 2007
+ * @license GNU/GPL
+ * @link http://www.creativepulse.eu
+ */
+
+// no direct access
+defined( '_JEXEC' ) or die( 'Restricted access' );
+
+echo $params->get('input');
+
+?>
---
+++ sitex-joomla3/mod_sitex/mod_sitex.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * mod_sitex
+ *
+ * Simple text module
+ *
+ * @version 1.0
+ * @author Creative Pulse
+ * @copyright Creative Pulse 2008-2012
+ * @license GNU/GPL
+ * @link http://www.creativepulse.gr
+ */
+
+// no direct access
+defined( '_JEXEC' ) or die( 'Restricted access' );
+
+echo $params->get('input');
+
+?>
